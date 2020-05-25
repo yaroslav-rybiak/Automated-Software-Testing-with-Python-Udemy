e1d09208230fbbdc0bff08e55b1560bd9f19346f
Yaroslav Rybiak <yaroslav.rybiak@gmail.com>
2020-05-25 07:30:58 +0200

    adding try/catch block

--- 03_getting_user_input/code.py
+++ 03_getting_user_input/code.py
@@ -1,2 +1,9 @@
-user_age = input("How old are you? ")
-print(f"You are {user_age} years old")
+while(True):
+    user_age = input("How old are you? ")
+    try:
+        years = int(user_age)
+        seconds = years * 365 * 24 * 60 * 60
+        print(f"You are {seconds} seconds old")
+        break
+    except ValueError:
+        print("Please input an integer")
