7f9774f4848309fdded374eb50dc59a5fa88da9d
Yaroslav Rybiak <yaroslav.rybiak@gmail.com>
2020-06-06 19:46:04 +0200

    guess the number

--- 17_in_keyword/code.py
+++ 17_in_keyword/code.py
@@ -1,4 +1,16 @@
-friends = ["Rolf", "Bob", "Anna"]
+#game 'guess the number'
+secret_number = 42
+user_input = input("Do you want to play? ").lower()
+if user_input in ("y", "yes"):
+    while True:
+        try:
+            user_number = int(input("Guess the number "))
+            while user_number != secret_number:
+                user_number = int(input("No! Guess again "))
+            print("Correct!")
+            break
+        except ValueError:
+            print("Please input an integer")
 
-if("Bob" in friends):
-    print("Hi, Bob!")
+else:
+    print("Have a nice day!")
